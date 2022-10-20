84e013889e5e3709ec5f44f89630f15b4396944e
swarabintank16 <swarabintank2@gmail.com>
2022-10-20 20:38:59 +0700

    bintang

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama saya Habib Fiqri
+ Saya tingga di daerah Bogor
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+	}
+}
